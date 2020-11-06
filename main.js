155cdd87e9107c19800575d3c350a597e3ff3748
alphanout <atishay818@gmail.com>
2020-11-06 20:37:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+let microBtn = document.getElementById("Micro");
+let setIntervalBtn = document.getElementById("SetInterval");
+let rafBtn = document.getElementById("RAF");
+let setTimeoutBtn = document.getElementById("SetTimeout");
+let clearBtn = document.getElementById("clear");
+
+let print = document.getElementById("result");
+
+microBtn.addEventListener("click", startMicro);
+rafBtn.addEventListener("click", startRaf);
+clearBtn.addEventListener("click", startCLear);
+setIntervalBtn.addEventListener("click", startSetInterval);
+setTimeoutBtn.addEventListener("click", startSetTimeOut);
+let id = null;
+
+function task(scheduler, res, name = "") {
+    let i = 0;
+    let start = Date.now();
+
+    function complexTask() {
+        do {
+            i++;
+        } while (i % 1e6 != 0);
+
+        if (i === 1e8 || i % 1e8 === 0) {
+            if (name === "setInterval") {
+                print.innerHTML += id + " hello" + "<br>";
+                clearInterval(id);
+            }
+            res.time = (Date.now() - start) / 1000;
+            print.innerHTML += `${scheduler.name + name + " " + id} completed in ${res.time} seconds` + "<br>";
+        } else {
+            if (scheduler !== "setInterval")
+                scheduler(complexTask);
+        }
+    }
+    console.log("started");
+    if (scheduler !== "setInterval") {
+        scheduler(complexTask);
+    }
+}
+
+function startMicro() {
+    task(queueMicrotask, {});
+}
+
+function startRaf() {
+    for (let j = 0; j < 1; j++) {
+        task(requestAnimationFrame, {});
+    }
+}
+
+function startSetInterval() {
+    task((run) => {
+        id = setInterval(() => {
+            run();
+        });
+    }, {}, "setInterval");
+}
+
+function startSetTimeOut() {
+    task((run) => {
+        id = setTimeout(() => {
+            run();
+        });
+    }, {}, "setTimeout");
+}
+
+function startCLear() {
+    print.innerHTML = "";
+}
